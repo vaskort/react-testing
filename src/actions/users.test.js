d6d36ef4f1a01f963cec21c00827df48dc38918a
Vasilis Kortsimelidis <vaskort@gmail.com>
2019-01-15 12:40:30 +0000

    Use await instead of promises

--- src/actions/users.test.js
+++ src/actions/users.test.js
@@ -16,45 +16,42 @@ describe("User Actions", () => {
   });
 
   describe("getUsers action creator", () => {
-    it("tests GET_USERS action and that returns data on success", done => {
+    it("dispatches GET_USERS action and returns data on success", async () => {
       mockAxios.get.mockImplementationOnce(() =>
         Promise.resolve({
           data: [{ id: 1, name: "Vasilis" }]
         })
       );
 
-      store.dispatch(getUsers()).then(() => {
-        const actions = store.getActions();
-        // [ { type: 'GET_USERS_PENDING' },
-        //   { type: 'GET_USERS_FULFILLED', payload: { data: [Array] } } 
-        // ]
+      await store.dispatch(getUsers());
+      const actions = store.getActions();
+      // [ { type: 'GET_USERS_PENDING' },
+      //   { type: 'GET_USERS_FULFILLED', payload: { data: [Array] } } 
+      // ]
 
-        expect.assertions(3);
-        expect(actions[0].type).toEqual("GET_USERS_PENDING");
-        expect(actions[1].type).toEqual("GET_USERS_FULFILLED");
-        expect(actions[1].payload.data[0].name).toEqual("Vasilis");
-
-        done();
-      });
+      expect.assertions(3);
+      expect(actions[0].type).toEqual("GET_USERS_PENDING");
+      expect(actions[1].type).toEqual("GET_USERS_FULFILLED");
+      expect(actions[1].payload.data[0].name).toEqual("Vasilis");
     });
 
-    it("tests GET_USERS action and that returns an error", done => {
+    it("tests GET_USERS action and that returns an error", async () => {
       mockAxios.get.mockImplementationOnce(() =>
         Promise.reject({
           error: "Something bad happened :("
         })
       );
-
-      store.dispatch(getUsers()).catch(() => {
+      
+      try { 
+        await store.dispatch(getUsers());
+      } catch {
         const actions = store.getActions();
 
         expect.assertions(3);
         expect(actions[0].type).toEqual("GET_USERS_PENDING");
         expect(actions[1].type).toEqual("GET_USERS_REJECTED");
         expect(actions[1].payload.error).toEqual("Something bad happened :(");
-
-        done();
-      });
+      }
     });
   });
 });
